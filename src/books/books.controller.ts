1b72c735608ae4f318d527f9bb1ab7ca0b84f3ba
monte-dev <badziobw@gmail.com>
2023-09-24 21:37:57 +0100

    Add: books Get endpoint logic

---
+++ src/books/books.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Controller,
+  Param,
+  Get,
+  ParseUUIDPipe,
+  NotFoundException,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+
+@Controller('books')
+export class BooksController {
+  constructor(private booksService: BooksService) {}
+
+  @Get('/')
+  getAll() {
+    return this.booksService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const book = await this.booksService.getById(id);
+    if (!book) throw new NotFoundException('Book not found in a database...');
+    return book;
+  }
+}
---
+++ src/books/books.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { Book } from '@prisma/client';
+import { PrismaService } from 'src/shared/services/prisma.service';
+
+@Injectable()
+export class BooksService {
+  constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<Book[]> {
+    return this.prismaService.book.findMany();
+  }
+
+  public getById(id: Book['id']): Promise<Book | null> {
+    return this.prismaService.book.findUnique({
+      where: { id },
+    });
+  }
+}
