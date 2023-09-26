54f8f784698150f5737937b2b44324ab86afa341
monte-dev <badziobw@gmail.com>
2023-09-26 12:53:15 +0100

    Add user registration endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() newUserData: RegisterDTO) {
+    return this.authService.register(newUserData);
+  }
 }
