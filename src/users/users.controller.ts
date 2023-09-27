fbcf699f27cba6d2725f333fa8a35af5441a7c13
monte-dev <badziobw@gmail.com>
2023-09-27 23:26:30 +0100

    add: roles using adminAuthGuard

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,9 +3,13 @@ import {
   Get,
   Param,
   ParseUUIDPipe,
+  UseGuards,
+  Delete,
   NotFoundException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -21,5 +25,14 @@ export class UsersController {
     const user = await this.usersService.getById(id);
     if (!user) throw new NotFoundException('User not found in database');
     return user;
+  }
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.usersService.getById(id)))
+      throw new NotFoundException(`User: ${id}  not found`);
+    await this.usersService.deleteById(id);
+    return { success: true };
   }
 }
