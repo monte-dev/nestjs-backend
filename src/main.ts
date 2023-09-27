488fdcd51d0781a2b51906cd5a72a1d074a80974
monte-dev <badziobw@gmail.com>
2023-09-27 23:16:51 +0100

    add configuration using COnfigService

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('api');
   app.use(cookieParser());
+  const configService = app.get(ConfigService);
 
   await app.enableShutdownHooks();
-  await app.listen(3000);
+  await app.listen(configService.get('port'));
 }
 bootstrap();
